abbc088d33fa32b6f615dc1fcaef7e12530341cf
Jason Stuber <gradhouse@pm.me>
2025-09-30 11:55:11 +0200

    add math commands (#9)

--- tests/latex/definitions/register/test_register_command_definitions.py
+++ tests/latex/definitions/register/test_register_command_definitions.py
@@ -9,6 +9,13 @@ from latex_parser.latex.definitions.command_definition_registry import CommandDe
 from latex_parser.latex.definitions.register.register_command_definitions import (
     register_document_commands,
     register_sectioning_commands,
+    register_greek_letter_commands,
+    register_binary_operation_commands,
+    register_relation_commands,
+    register_arrow_commands,
+    register_misc_symbol_commands,
+    register_variable_sized_symbol_commands,
+    register_log_like_function_commands,
     register_latex_commands
 )
 
@@ -53,6 +60,156 @@ class TestRegisterSectioningCommands:
         assert registered_keys == expected_commands, f"Expected {expected_commands}, got {registered_keys}"
 
 
+class TestRegisterGreekLetterCommands:
+    """Test register_greek_letter_commands function."""
+
+    def test_registers_expected_greek_letter_commands(self):
+        """Test that all expected Greek letter commands are registered and only those."""
+        registry = CommandDefinitionRegistry()
+        register_greek_letter_commands(registry)
+        
+        expected_commands = {
+            '\\Delta', '\\Gamma', '\\Lambda', '\\Omega', '\\Phi', '\\Pi', '\\Psi', '\\Sigma', '\\Theta', '\\Upsilon', '\\Xi', 
+            '\\alpha', '\\beta', '\\chi', '\\delta', '\\epsilon', '\\eta', '\\gamma', '\\iota', '\\kappa', '\\lambda', 
+            '\\mu', '\\nu', '\\omega', '\\phi', '\\pi', '\\psi', '\\rho', '\\sigma', '\\tau', '\\theta', '\\upsilon', 
+            '\\varepsilon', '\\varphi', '\\varpi', '\\varrho', '\\varsigma', '\\vartheta', '\\xi', '\\zeta'
+        }
+        
+        registered_keys = set(registry.list_keys())
+        
+        # Check that exactly the expected commands are present
+        assert registered_keys == expected_commands, f"Expected {expected_commands}, got {registered_keys}"
+
+
+class TestRegisterBinaryOperationCommands:
+    """Test register_binary_operation_commands function."""
+
+    def test_registers_expected_binary_operation_commands(self):
+        """Test that all expected binary operation commands are registered and only those."""
+        registry = CommandDefinitionRegistry()
+        register_binary_operation_commands(registry)
+        
+        expected_commands = {
+            '\\amalg', '\\ast', '\\bigcirc', '\\bigtriangledown', '\\bigtriangleup', '\\bullet', '\\cap', '\\cdot', 
+            '\\circ', '\\cup', '\\dagger', '\\ddagger', '\\diamond', '\\div', '\\lhd', '\\mp', '\\odot', '\\ominus', 
+            '\\oplus', '\\oslash', '\\otimes', '\\pm', '\\rhd', '\\setminus', '\\sqcap', '\\sqcup', '\\star', 
+            '\\times', '\\triangleleft', '\\triangleright', '\\unlhd', '\\unrhd', '\\uplus', '\\vee', '\\wedge', '\\wr'
+        }
+        
+        registered_keys = set(registry.list_keys())
+        
+        # Check that exactly the expected commands are present
+        assert registered_keys == expected_commands, f"Expected {expected_commands}, got {registered_keys}"
+
+
+class TestRegisterRelationCommands:
+    """Test register_relation_commands function."""
+
+    def test_registers_expected_relation_commands(self):
+        """Test that all expected relation commands are registered and only those."""
+        registry = CommandDefinitionRegistry()
+        register_relation_commands(registry)
+        
+        expected_commands = {
+            '\\Join', '\\approx', '\\asymp', '\\bowtie', '\\cong', '\\dashv', '\\doteq', '\\equiv', '\\frown', 
+            '\\geq', '\\gg', '\\in', '\\leq', '\\ll', '\\mid', '\\models', '\\neq', '\\ni', '\\notin', 
+            '\\parallel', '\\perp', '\\prec', '\\preceq', '\\propto', '\\sim', '\\simeq', '\\smile', 
+            '\\sqsubset', '\\sqsubseteq', '\\sqsupset', '\\sqsupseteq', '\\subset', '\\subseteq', '\\succ', 
+            '\\succeq', '\\vdash'
+        }
+        
+        registered_keys = set(registry.list_keys())
+        
+        # Check that exactly the expected commands are present
+        assert registered_keys == expected_commands, f"Expected {expected_commands}, got {registered_keys}"
+
+
+class TestRegisterArrowCommands:
+    """Test register_arrow_commands function."""
+
+    def test_registers_expected_arrow_commands(self):
+        """Test that all expected arrow commands are registered and only those."""
+        registry = CommandDefinitionRegistry()
+        register_arrow_commands(registry)
+        
+        expected_commands = {
+            '\\Downarrow', '\\Leftarrow', '\\Leftrightarrow', '\\Longleftarrow', '\\Longleftrightarrow', 
+            '\\Longrightarrow', '\\Rightarrow', '\\Uparrow', '\\Updownarrow', '\\downarrow', '\\hookleftarrow', 
+            '\\hookrightarrow', '\\leadsto', '\\leftarrow', '\\leftharpoondown', '\\leftharpoonup', 
+            '\\leftrightarrow', '\\longleftarrow', '\\longleftrightarrow', '\\longmapsto', '\\longrightarrow', 
+            '\\mapsto', '\\nearrow', '\\nwarrow', '\\rightarrow', '\\rightharpoondown', '\\rightharpoonup', 
+            '\\rightleftharpoons', '\\searrow', '\\swarrow', '\\uparrow', '\\updownarrow'
+        }
+        
+        registered_keys = set(registry.list_keys())
+        
+        # Check that exactly the expected commands are present
+        assert registered_keys == expected_commands, f"Expected {expected_commands}, got {registered_keys}"
+
+
+class TestRegisterMiscSymbolCommands:
+    """Test register_misc_symbol_commands function."""
+
+    def test_registers_expected_misc_symbol_commands(self):
+        """Test that all expected miscellaneous symbol commands are registered and only those."""
+        registry = CommandDefinitionRegistry()
+        register_misc_symbol_commands(registry)
+        
+        expected_commands = {
+            '\\Box', '\\Diamond', '\\Im', '\\Re', '\\aleph', '\\angle', '\\backslash', '\\bot', '\\clubsuit', 
+            '\\diamondsuit', '\\ell', '\\emptyset', '\\exists', '\\flat', '\\forall', '\\hbar', '\\heartsuit', 
+            '\\imath', '\\infty', '\\jmath', '\\mho', '\\nabla', '\\natural', '\\neg', '\\partial', '\\prime', 
+            '\\sharp', '\\spadesuit', '\\surd', '\\top', '\\triangle', '\\wp', '\\|'
+        }
+        
+        registered_keys = set(registry.list_keys())
+        
+        # Check that exactly the expected commands are present
+        assert registered_keys == expected_commands, f"Expected {expected_commands}, got {registered_keys}"
+
+
+class TestRegisterVariableSizedSymbolCommands:
+    """Test register_variable_sized_symbol_commands function."""
+
+    def test_registers_expected_variable_sized_symbol_commands(self):
+        """Test that all expected variable-sized symbol commands are registered and only those."""
+        registry = CommandDefinitionRegistry()
+        register_variable_sized_symbol_commands(registry)
+        
+        expected_commands = {
+            '\\bigcap', '\\bigcup', '\\bigodot', '\\bigoplus', '\\bigotimes', '\\biguplus', '\\bigvee', 
+            '\\bigwedge', '\\coprod', '\\int', '\\oint', '\\prod', '\\sum'
+        }
+        
+        registered_keys = set(registry.list_keys())
+        
+        # Check that exactly the expected commands are present
+        assert registered_keys == expected_commands, f"Expected {expected_commands}, got {registered_keys}"
+
+
+class TestRegisterLogLikeFunctionCommands:
+    """Test register_log_like_function_commands function."""
+
+    def test_registers_expected_log_like_function_commands(self):
+        """Test that all expected log-like function commands are registered and only those."""
+        registry = CommandDefinitionRegistry()
+        register_log_like_function_commands(registry)
+        
+        expected_commands = {
+            '\\Pr', '\\arccos', '\\arcsin', '\\arctan', '\\arg', '\\bmod', '\\cos', '\\cosh', '\\cot', '\\coth', 
+            '\\csc', '\\deg', '\\det', '\\dim', '\\exp', '\\gcd', '\\hom', '\\inf', '\\ker', '\\lg', '\\lim', 
+            '\\liminf', '\\limsup', '\\ln', '\\log', '\\max', '\\min', '\\pmod', '\\sec', '\\sin', '\\sinh', 
+            '\\sup', '\\tan', '\\tanh'
+        }
+        
+        registered_keys = set(registry.list_keys())
+        
+        # Check that exactly the expected commands are present
+        assert registered_keys == expected_commands, f"Expected {expected_commands}, got {registered_keys}"
+
+
+    """Test register_latex_commands function."""
+
 class TestRegisterLatexCommands:
     """Test register_latex_commands function."""
 
@@ -61,7 +218,7 @@ class TestRegisterLatexCommands:
         registry = CommandDefinitionRegistry()
         register_latex_commands(registry)
         
-        # Combined set of all expected commands
+        # Combined set of all expected commands from all registration functions
         expected_commands = {
             # Document commands
             '\\documentclass', '\\documentstyle', '\\usepackage', '\\maketitle',
@@ -70,13 +227,49 @@ class TestRegisterLatexCommands:
             '\\part', '\\part*', '\\chapter', '\\chapter*',
             '\\section', '\\section*', '\\subsection', '\\subsection*',
             '\\subsubsection', '\\subsubsection*', '\\paragraph', '\\paragraph*',
-            '\\subparagraph', '\\subparagraph*'
+            '\\subparagraph', '\\subparagraph*',
+            # Greek letter commands
+            '\\Delta', '\\Gamma', '\\Lambda', '\\Omega', '\\Phi', '\\Pi', '\\Psi', '\\Sigma', '\\Theta', '\\Upsilon', '\\Xi', 
+            '\\alpha', '\\beta', '\\chi', '\\delta', '\\epsilon', '\\eta', '\\gamma', '\\iota', '\\kappa', '\\lambda', 
+            '\\mu', '\\nu', '\\omega', '\\phi', '\\pi', '\\psi', '\\rho', '\\sigma', '\\tau', '\\theta', '\\upsilon', 
+            '\\varepsilon', '\\varphi', '\\varpi', '\\varrho', '\\varsigma', '\\vartheta', '\\xi', '\\zeta',
+            # Binary operation commands
+            '\\amalg', '\\ast', '\\bigcirc', '\\bigtriangledown', '\\bigtriangleup', '\\bullet', '\\cap', '\\cdot', 
+            '\\circ', '\\cup', '\\dagger', '\\ddagger', '\\diamond', '\\div', '\\lhd', '\\mp', '\\odot', '\\ominus', 
+            '\\oplus', '\\oslash', '\\otimes', '\\pm', '\\rhd', '\\setminus', '\\sqcap', '\\sqcup', '\\star', 
+            '\\times', '\\triangleleft', '\\triangleright', '\\unlhd', '\\unrhd', '\\uplus', '\\vee', '\\wedge', '\\wr',
+            # Relation commands
+            '\\Join', '\\approx', '\\asymp', '\\bowtie', '\\cong', '\\dashv', '\\doteq', '\\equiv', '\\frown', 
+            '\\geq', '\\gg', '\\in', '\\leq', '\\ll', '\\mid', '\\models', '\\neq', '\\ni', '\\notin', 
+            '\\parallel', '\\perp', '\\prec', '\\preceq', '\\propto', '\\sim', '\\simeq', '\\smile', 
+            '\\sqsubset', '\\sqsubseteq', '\\sqsupset', '\\sqsupseteq', '\\subset', '\\subseteq', '\\succ', 
+            '\\succeq', '\\vdash',
+            # Arrow commands
+            '\\Downarrow', '\\Leftarrow', '\\Leftrightarrow', '\\Longleftarrow', '\\Longleftrightarrow', 
+            '\\Longrightarrow', '\\Rightarrow', '\\Uparrow', '\\Updownarrow', '\\downarrow', '\\hookleftarrow', 
+            '\\hookrightarrow', '\\leadsto', '\\leftarrow', '\\leftharpoondown', '\\leftharpoonup', 
+            '\\leftrightarrow', '\\longleftarrow', '\\longleftrightarrow', '\\longmapsto', '\\longrightarrow', 
+            '\\mapsto', '\\nearrow', '\\nwarrow', '\\rightarrow', '\\rightharpoondown', '\\rightharpoonup', 
+            '\\rightleftharpoons', '\\searrow', '\\swarrow', '\\uparrow', '\\updownarrow',
+            # Miscellaneous symbol commands
+            '\\Box', '\\Diamond', '\\Im', '\\Re', '\\aleph', '\\angle', '\\backslash', '\\bot', '\\clubsuit', 
+            '\\diamondsuit', '\\ell', '\\emptyset', '\\exists', '\\flat', '\\forall', '\\hbar', '\\heartsuit', 
+            '\\imath', '\\infty', '\\jmath', '\\mho', '\\nabla', '\\natural', '\\neg', '\\partial', '\\prime', 
+            '\\sharp', '\\spadesuit', '\\surd', '\\top', '\\triangle', '\\wp', '\\|',
+            # Variable-sized symbol commands
+            '\\bigcap', '\\bigcup', '\\bigodot', '\\bigoplus', '\\bigotimes', '\\biguplus', '\\bigvee', 
+            '\\bigwedge', '\\coprod', '\\int', '\\oint', '\\prod', '\\sum',
+            # Log-like function commands
+            '\\Pr', '\\arccos', '\\arcsin', '\\arctan', '\\arg', '\\bmod', '\\cos', '\\cosh', '\\cot', '\\coth', 
+            '\\csc', '\\deg', '\\det', '\\dim', '\\exp', '\\gcd', '\\hom', '\\inf', '\\ker', '\\lg', '\\lim', 
+            '\\liminf', '\\limsup', '\\ln', '\\log', '\\max', '\\min', '\\pmod', '\\sec', '\\sin', '\\sinh', 
+            '\\sup', '\\tan', '\\tanh'
         }
         
         registered_keys = set(registry.list_keys())
         
         # Check that exactly the expected commands are present
-        assert registered_keys == expected_commands, f"Expected {expected_commands}, got {registered_keys}"
+        assert registered_keys == expected_commands, f"Expected {len(expected_commands)} commands, got {len(registered_keys)}. Missing: {expected_commands - registered_keys}, Extra: {registered_keys - expected_commands}"
 
     def test_command_consistency_across_runs(self):
         """Test that the same commands are registered consistently across multiple runs."""
@@ -92,3 +285,16 @@ class TestRegisterLatexCommands:
         
         # Should be identical
         assert keys1 == keys2, "Command registration is not consistent across runs"
+    
+    def test_expected_total_command_count(self):
+        """Test that the total number of registered commands matches expectations."""
+        registry = CommandDefinitionRegistry()
+        register_latex_commands(registry)
+        
+        # Expected counts based on individual function tests:
+        # Document: 8, Sectioning: 14, Greek: 40, Binary: 36, Relation: 36, 
+        # Arrow: 32, Misc: 33, Variable-sized: 13, Log-like: 34
+        expected_total = 8 + 14 + 40 + 36 + 36 + 32 + 33 + 13 + 34
+        actual_total = len(registry.list_keys())
+        
+        assert actual_total == expected_total, f"Expected {expected_total} total commands, got {actual_total}"
